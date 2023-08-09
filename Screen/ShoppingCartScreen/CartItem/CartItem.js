0b181963d7b7a80fa189ddcdda052941ad0a3322
Md Nuruddin <nuruddinalways99@gmail.com>
2023-08-09 20:15:10 +0600

    Checkout view

--- Screen/ShoppingCartScreen/CartItem/CartItem.js
+++ Screen/ShoppingCartScreen/CartItem/CartItem.js
@@ -1,43 +1,51 @@
-import { EvilIcons } from "@expo/vector-icons";
+import { AntDesign, EvilIcons } from "@expo/vector-icons";
 import { Image } from "expo-image";
-import { Pressable, Text, View } from "react-native";
+import { useState } from "react";
+import { Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const CartItem = ({ title, price, category, image }) => {
+  const [quantity, setQuantity] = useState(0);
+
   return (
     <View style={{ flexDirection: "row", alignItems: "center" }}>
       <View style={{ flex: 4, flexDirection: "row", alignItems: "center" }}>
         <View style={{ flex: 1, marginRight: 20 }}>
           <Image
             source={image}
+            contentFit='cover'
             style={{
               width: 80,
               height: 80,
-              resizeMode: "contain",
               borderRadius: 5,
             }}
           />
         </View>
         <View style={{ flex: 3 }}>
-          <Text style={{ fontSize: 16, fontWeight: "500" }}>{title}</Text>
+          <Text style={{ fontSize: 16, fontWeight: "500" }}>
+            {title.slice(0, 20)}
+          </Text>
           <Text>{category}</Text>
-          <Text style={{ fontWeight: "500" }}>${price}</Text>
+          <View style={{ flexDirection: "row", alignItems: "center", gap: 3 }}>
+            <Text style={{ fontWeight: "500" }}>${price}.00</Text>
+            <AntDesign name='close' size={16} />
+            <Text style={{ fontSize: 14, fontWeight: "500" }}>{quantity}</Text>
+          </View>
         </View>
       </View>
-      <View style={{ flexDirection: "column", alignItems: "center", gap: 5 }}>
-        <EvilIcons color={"red"} name='trash' size={28} />
-        <Pressable>
-          <Text
-            style={{
-              backgroundColor: "dodgerblue",
-              paddingVertical: 5,
-              paddingHorizontal: 10,
-              borderRadius: 5,
-              color: "#fff",
-            }}
+      <View style={{ flexDirection: "column", alignItems: "center", gap: 10 }}>
+        <EvilIcons color={"red"} name='trash' size={34} />
+        <View style={{ flexDirection: "row", alignItems: "center", gap: 5 }}>
+          <TouchableOpacity onPress={() => setQuantity(quantity + 1)}>
+            <AntDesign name='pluscircleo' size={20} />
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            onPress={() => setQuantity(quantity > 0 ? quantity - 1 : 0)}
           >
-            Checkout
-          </Text>
-        </Pressable>
+            <AntDesign name='minuscircleo' size={20} />
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
--- Screen/ShoppingCartScreen/ShoppingCart.js
+++ Screen/ShoppingCartScreen/ShoppingCart.js
@@ -1,6 +1,7 @@
 import { useNavigation } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { FlatList, Pressable, Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Back from "../../components/BackButton/Back";
 import CartItem from "./CartItem/CartItem";
@@ -67,7 +68,7 @@ const ShoppingCart = () => {
   ];
 
   const renderItem = ({ item }) => (
-    <View style={{ marginVertical: 10}}>
+    <View style={{ marginVertical: 10 }}>
       <CartItem {...item} />
     </View>
   );
@@ -95,14 +96,57 @@ const ShoppingCart = () => {
       {/* Back Button Ends */}
 
       {/* Cart Item View */}
-      <View style={{ marginHorizontal: 10, marginTop: 20 }}>
+      <View style={{ marginHorizontal: 10, marginTop: 14 }}>
         <FlatList
-          data={products}
+          data={products.slice(0, 5)}
           renderItem={renderItem}
           keyExtractor={(item) => item.id}
         />
       </View>
       {/* Cart Item View Ends */}
+
+      {/* Checkout View */}
+      <View style={{ marginHorizontal: 10, marginTop: 20 }}>
+        <Text style={{ fontSize: 20, fontWeight: "500" }}>Order Info</Text>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            marginTop: 10,
+          }}
+        >
+          <View>
+            <Text>Subtotal:</Text>
+            <Text>Delivery:</Text>
+            <Text style={{ fontSize: 16, fontWeight: "bold", marginTop: 2 }}>
+              Total:
+            </Text>
+          </View>
+          <View>
+            <Text style={{ textAlign: "center" }}>$430.00</Text>
+            <Text style={{ textAlign: "center" }}>$50.00</Text>
+            <View style={{ borderWidth: 0.5, marginVertical: 2 }}></View>
+            <Text style={{ fontSize: 16, fontWeight: "bold" }}>$480.00</Text>
+          </View>
+        </View>
+        <TouchableOpacity>
+          <Text
+            style={{
+              textAlign: "center",
+              backgroundColor: "#000",
+              color: "#fff",
+              fontSize: 16,
+              fontWeight: "500",
+              marginTop: 14,
+              paddingVertical: 10,
+              borderRadius: 5,
+            }}
+          >
+            Checkout
+          </Text>
+        </TouchableOpacity>
+      </View>
+      {/* Checkout View Ends */}
 
       <StatusBar style='auto' />
     </SafeAreaView>
