3c36ce5c7718cf0d8201d48eab1e8fe78f419065
Md Nuruddin <nuruddinalways99@gmail.com>
2023-08-18 21:36:30 +0600

    Add Redux structure

--- redux/products/actionTypes.js
+++
@@ -1,4 +0,0 @@
-export const ADD_TO_CART = "product/add_to_cart";
-export const QTY_INC = "product/qty_inc";
-export const QTY_DEC = "product/qty_dec";
-export const ADD_TO_WISHLIST = "product/add_to_wishlist";
